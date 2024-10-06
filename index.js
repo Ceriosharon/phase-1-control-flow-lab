3d7a0fb684080528ff663402c319b4d6334d52ab
ceriosharon <ccerio73@gmail.com>
2024-10-06 12:51:16 +0300

    controlflowlab

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
-function scuberGreetingForFeet(distance) {
-  if (distance <= 400) {
+function scuberGreetingForFeet(feet){
+  if(feet <= 400){
     return 'This one is on me!';
-  } else if (distance > 400 && distance <= 2000) {
+  }
+  else if(feet < 400 && feet <= 2000){
+    return('That will be 20 bucks.');
+  }
+  else if( feet < 2000 && feet <= 2500){
     return 'That will be twenty bucks.';
-  } else if (distance > 2000 && distance <= 2500) {
+  }
+   else if( feet < 2500 && feet <=3500){
     return 'I will gladly take your thirty bucks.';
-  } else {
+  }else{
     return 'No can do.';
+}
+  
   }
-}
 
-function ternaryCheckCity(city) {
-  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
-}
 
-function switchOnCharmFromTip(tip) {
-  switch (tip) {
-    case 'generous':
-      return 'Thank you so much.';
-    case 'not as generous':
-      return 'Thank you.';
-    case 'cheap':
-      return 'Bye.';
-    default:
-      return 'Bye.';
-  }
+  function ternaryCheckCity(city){
+  // Write your code here!
+if(city => NYC){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
-
+}
+ 
+function switchOnCharmFromTip(tip){
+  // Write your code here 
+switch(tip){
+  case 'generous':
+    return 'Thank you so much.';
+    break;
+  case 'not as generous':
+    return 'Thank you.';
+    break;
+  default:
+    return 'Bye.';
+}
+}
